0389d359afdb0ca5452dfbdf00b1c2330117b3f4
Arya Vatan-Abadi <arya.vatanabadi@gmail.com>
2020-04-13 14:15:47 -0400

    Color Selector
    Menu bar added with a menu for color selection, also added a file menu with the option to clear the canvas. Noticed a strange bug were if the window is resized all drawings are erased.

--- Sketchpad.java
+++ Sketchpad.java
@@ -1,13 +1,12 @@
 import java.awt.*;
-import java.awt.event.MouseEvent;
 import java.awt.event.*;
 
-class Sketchpad extends Frame {
+class Sketchpad extends Frame implements ActionListener {
     private static final long serialVersionUID = 1L;
 
     // Main Method
     public static void main(String args[]) {
-        new Sketchpad().setVisible(true);
+        new Sketchpad();
     }
 
     // Sketchpad Constructor
@@ -16,26 +15,53 @@ class Sketchpad extends Frame {
         setTitle("Simple Sketchpad");
         setSize(1000, 800);
         setLayout(null);
+        setVisible(true);
 
-        // Tool Panel
+        // Panel
         Panel panel = new Panel();
-        panel.setBounds(0,30, this.getWidth()+3000, 40);
-        panel.setBackground(Color.darkGray);
+        panel.setBounds(0, 30, this.getWidth() + 3000, 40);
+        panel.setBackground(Color.lightGray);
         this.add(panel);
+
+        // Menu Bar
+        MenuBar bar = new MenuBar();
+        Menu fileMenu = new Menu("File");
+        MenuItem clear = new MenuItem("Clear");
+        clear.addActionListener(this);
+        fileMenu.add(clear);
+        bar.add(fileMenu);
+        Menu colorMenu = new Menu("Color");
+        MenuItem black = new MenuItem("Black");
+        black.addActionListener(this);
+        MenuItem blue = new MenuItem("Blue");
+        blue.addActionListener(this);
+        MenuItem green = new MenuItem("Green");
+        green.addActionListener(this);
+        MenuItem red = new MenuItem("Red");
+        red.addActionListener(this);
+        MenuItem yellow = new MenuItem("Yellow");
+        yellow.addActionListener(this);
+        colorMenu.add(black);
+        colorMenu.add(red);
+        colorMenu.add(green);
+        colorMenu.add(blue);
+        colorMenu.add(yellow);
+        bar.add(colorMenu);
+        setMenuBar(bar);
 
         // Event Listeners
         addMouseListener(new myMouseHandler());
         addMouseMotionListener(new myMouseMotionHandler());
-        addWindowListener(new WindowAdapter(){  // Closing the window
-            public void windowClosing(WindowEvent e) {  
-                dispose();  
-            }  
-        });  
-
+        addWindowListener(new WindowAdapter() { // Closing the window
+            public void windowClosing(WindowEvent e) {
+                dispose();
+            }
+        });
     }
 
     // Global Variables
     DrawMode drawMode = DrawMode.FREEHAND;
+    Color color = Color.BLACK;
     int x0, y0, x, y;
 
     // Mouse Handler
@@ -55,19 +81,46 @@ class Sketchpad extends Frame {
         public void mouseMoved(MouseEvent e) { }
 
         public void mouseDragged(MouseEvent e) {
+            Graphics g = getGraphics();
+            g.setColor(color);
+
             switch (drawMode) {
                 case FREEHAND:
                     x0 = x;
                     y0 = y;
                     x = e.getX();
                     y = e.getY();
-                    Graphics g = getGraphics();
-                    g.setColor(Color.blue);
                     g.drawLine(x0, y0, x, y);
                     break;
                 default:
                     break;
             }
+        }
+    }
+
+    // Action Listener
+    public void actionPerformed(ActionEvent e) {
+        // Clear Canvas
+        if(e.getActionCommand().equals("Clear")){
+            repaint();
+            return;
+        }
+
+        // Color Selection
+        if(e.getActionCommand().equals("Black")){
+            this.color = Color.BLACK;
+        }
+        else if(e.getActionCommand().equals("Blue")){
+            this.color = Color.BLUE;
+        }
+        else if(e.getActionCommand().equals("Green")){
+            this.color = Color.GREEN;
+        }
+        else if(e.getActionCommand().equals("Red")){
+            this.color = Color.RED;
+        }
+        else if(e.getActionCommand().equals("Yellow")){
+            this.color = Color.YELLOW;
         }
     }
 }
